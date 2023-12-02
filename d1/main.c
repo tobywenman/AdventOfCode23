39c22d713017fb1ed253b25b68a23921f181ba86
TobyWenman <toby.wenman@gmail.com>
2023-12-02 12:58:23 +0000

    done day 1

--- d1/main.c
+++ d1/main.c
@@ -4,8 +4,9 @@
 #include <stdbool.h>
 
 const char *templates[18] = {"1","2","3","4","5","6","7","8","9","one","two","three","four","five","six","seven","eight","nine"};
+const size_t sizes[18] = {1,1,1,1,1,1,1,1,1,3,3,5,4,4,3,5,5,4};
 
-unsigned checkLine(char* line, size_t size)
+unsigned checkLineForward(char* line, size_t size)
 {
     unsigned pos[18];
     for(unsigned i=0; i<18; i++)
@@ -18,7 +19,22 @@ unsigned checkLine(char* line, size_t size)
             if (line[i] == templates[j][pos[j]])
             {
                 pos[j]++;
-                if (templates[j][pos[j]] == 0)
+                if (pos[j] == sizes[j])
+                {
+                    if(j<=8)
+                    {
+                        return j+1;
+                    }
+                    else
+                    {
+                        return j-8;
+                    }
+                }
+            }
+            else if (line[i] == templates[j][0])
+            {
+                pos[j] = 1;
+                if (pos[j] == sizes[j])
                 {
                     if(j<=8)
                     {
@@ -38,6 +54,43 @@ unsigned checkLine(char* line, size_t size)
     }
 }
 
+unsigned checkLineBackward(char* line, size_t size)
+{
+    unsigned pos[18];
+    for(unsigned i=0; i<18; i++)
+        pos[i]=sizes[i]-1;
+
+    for (unsigned i=size; i-->0;)
+    {
+        for (unsigned j=0; j<18; j++)
+        {
+            if (line[i] == templates[j][pos[j]])
+            {
+                if (pos[j] == 0)
+                {
+                    if(j<=8)
+                    {
+                        return j+1;
+                    }
+                    else
+                    {
+                        return j-8;
+                    }
+                }
+                pos[j]--;
+            }
+            else if(line[i] == templates[j][sizes[j]-1])
+            {
+                pos[j] = sizes[j]-2;
+            }
+            else
+            {
+                pos[j] = sizes[j]-1;
+            }
+        }
+    }
+}
+
 int main()
 {
     FILE *fp;
@@ -50,16 +103,23 @@ int main()
     line = malloc(bufSize);
 
     int strLen;
-    unsigned count = 1;
+    unsigned count = 0;
+    unsigned test=1;
     while (true)
     {
+        unsigned lineVal = 0;
         strLen = getline(&line, &bufSize, fp);
         if (strLen <= 0)
         {
             break;
         }
 
-        printf("%u val: %u\n", count, checkLine(line, strLen));
-        count++;
+        lineVal += checkLineBackward(line, strLen);
+        lineVal += 10*checkLineForward(line,strLen);
+        line[strLen-1] = 0;
+        test++;
+        count += lineVal;
     } 
+    free(line);
+    printf("%u\n", count);
 }
