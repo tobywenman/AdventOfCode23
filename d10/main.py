afe741d2e352a34fb8939baba3569402075da173
Toby Wenman <tobywenman@gmail.com>
2023-12-10 19:23:48 +0000

    d10 p2

--- d10/main.py
+++ d10/main.py
@@ -112,9 +112,67 @@ def getInsideDir(path):
             elif i[2] == 0:
                 right += 1
         prevDir = i[2]
-    print(left, right)
     
     return left > right
+
+def fill(grid, node, pathSet):
+    print("testing:",node)
+    if (grid[node[0]][node[1]] == 'I' or node in pathSet):
+        return
+    print("found")
+    grid[node[0]][node[1]] = "I"
+    if node[0] != 0:
+        fill(grid, (node[0]-1,node[1]),pathSet)
+    
+    if node[0] != len(grid)-1:
+        fill(grid, (node[0]+1,node[1]),pathSet)
+
+    if node[1] != 0:
+        fill(grid, (node[0],node[1]-1),pathSet)
+
+    if node[1] != len(grid[0])-1:
+        fill(grid, (node[0],node[1]+1),pathSet)
+    
+    return
+
+
+
+def fillAll(path, grid, insideDir):
+    pathSet = set()
+
+    for i in path:
+        pathSet.add((i[0],i[1]))
+
+    for i in path:
+        if insideDir:
+            if i[2] == 0:
+                if i[1] != 0:
+                    fill(grid, (i[0], i[1]-1), pathSet)
+            elif i[2] == 1:
+                if i[1] != len(grid[0])-1:
+                    fill(grid, (i[0], i[1]+1), pathSet)
+            elif i[2] == 2:
+                if i[0] != 0:
+                    fill(grid, (i[0], i[1]-1), pathSet)
+            elif i[2] == 3:
+                if i[0] != len(grid)-1:
+                    fill(grid, (i[0], i[1]+1), pathSet)
+        else:
+            if i[2] == 1:
+                if i[1] != 0:
+                    fill(grid, (i[0], i[1]-1), pathSet)
+            elif i[2] == 0:
+                if i[1] != len(grid[0])-1:
+                    fill(grid, (i[0], i[1]+1), pathSet)
+            elif i[2] == 3:
+                if i[0] != 0:
+                    fill(grid, (i[0]-1, i[1]), pathSet)
+            elif i[2] == 2:
+                if i[0] != len(grid)-1:
+                    fill(grid, (i[0]+1, i[1]), pathSet)
+
+                
+
 
 
 with open("data.txt", "r") as file:
@@ -134,8 +192,16 @@ with open("data.txt", "r") as file:
 
     route = startRoute(entry, data)
 
-    print(route)
-    print("mid num:", len(route)//2)
-    print("mid:",route[len(route)//2])
+    # print(route)
+    # print("mid num:", len(route)//2)
+    # print("mid:",route[len(route)//2])
 
-    print(getInsideDir(route))
+    fillAll(route, data, getInsideDir(route))
+
+    total = 0
+    for i in data:
+        for j in i:
+            print(j, end="")
+        print()
+        total += i.count("I")
+
