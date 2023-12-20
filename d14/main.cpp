3c480fa3e7615c069a7d65289e74e43f1f1439d1
TobyWenman <toby.wenman@gmail.com>
2023-12-20 19:42:23 +0000

    d14 done

--- d14/main.cpp
+++ d14/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include <cassert>
+#include <unordered_map>
 
 using grid = std::vector<std::vector<char>>;
 
@@ -66,6 +67,79 @@ void rollNorth(grid &in)
     }
 }
 
+void rollWest(grid &in)
+{
+    for (unsigned i=1; i<in[0].size(); i++)
+    {
+        for (unsigned j=0; j<in.size(); j++)
+        {
+            if (in[j][i] == 'O')
+            {
+                in[j][i] = '.';
+                unsigned end=0;
+                for (unsigned k=i; k>0; k--)
+                {
+                    if (in[j][k-1] != '.')
+                    {
+                        end = k;
+                        break;
+                    }
+                }
+                in[j][end] = 'O';
+            }
+        }
+    }
+}
+
+void rollSouth(grid &in)
+{
+    for (unsigned i=in.size()-1; i-->0;)
+    {
+        for (unsigned j=0; j<in[0].size(); j++)
+        {
+            if (in[i][j] == 'O')
+            {
+                in[i][j] = '.';
+                unsigned end=in.size()-1;
+                
+                for (unsigned k=i; k<in.size()-1; k++)
+                {
+                    if (in[k+1][j] != '.')
+                    {
+                        end = k;
+                        break;
+                    }
+                }
+                in[end][j] = 'O';
+            }
+        }
+    }
+}
+
+void rollEast(grid &in)
+{
+    for (unsigned i=in[0].size()-1; i-->0;)
+    {
+        for (unsigned j=0; j<in.size(); j++)
+        {
+            if (in[j][i] == 'O')
+            {
+                in[j][i] = '.';
+                unsigned end=in[0].size()-1;
+                for (unsigned k=i; k<in[0].size()-1; k++)
+                {
+                    if (in[j][k+1] != '.')
+                    {
+                        end = k;
+                        break;
+                    }
+                }
+                in[j][end] = 'O';
+            }
+        }
+    }
+}
+
 unsigned countWeight(const grid &in)
 {
     unsigned weight=0;
@@ -83,6 +157,34 @@ unsigned countWeight(const grid &in)
     return weight;
 }
 
+std::vector<bool> hashGrid(const grid &in)
+{
+    std::vector<bool> out;
+    for (unsigned i=0; i<in.size(); i++)
+    {
+        for (unsigned j=0; j<in[0].size(); j++)
+        {
+            if (in[i][j]=='O')
+            {
+                out.push_back(true);
+            }
+            else
+            {
+                out.push_back(false);
+            }
+        }
+    }
+    return out;
+}
+
+void spin(grid &in)
+{
+    rollNorth(in);
+    rollWest(in);
+    rollSouth(in);
+    rollEast(in);
+}
+
 int main()
 {
     grid test;
@@ -91,8 +193,36 @@ int main()
     
     uint64_t total=0;
     readGrid(ifs, test);
-    rollNorth(test);
+    
+    std::unordered_map<std::vector<bool>, unsigned> stepCache;
+
+    unsigned step=0;
+    while (true)
+    {
+        std::vector<bool> hash = hashGrid(test);
+
+        if (stepCache.count(hash))
+        {
+            unsigned remaining = (1000000000 - stepCache[hash])%(step - stepCache[hash]);
+            std::cout << "remaining: " << remaining << '\n';
+            std::cout << "steps: " << step << '\n';
+            std::cout << "loop size: " << step-stepCache[hash] << '\n';
+
+            for (unsigned i=0; i<remaining; i++)
+            {
+                spin(test);
+            }
+            break;
+        }
+
+        stepCache[hash] = step;
+        spin(test);
+        step++;
+    }
+
     total = countWeight(test);
+
+    std::cout << "size: " << test.size() << ',' << test[0].size() << '\n';
 
     std::cout << "weight: " << total << '\n';
 }
